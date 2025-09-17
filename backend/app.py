6d413f0fe44ae1820e2d54ef500f1b713f14a202
SaiSupraja2004 <145634083+SaiSupraja2004@users.noreply.github.com>
2025-09-17 13:51:40 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -218,7 +218,10 @@ def hospitals():
     except Exception as e:
         traceback.print_exc()
         return jsonify({'error': str(e)}), 500
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
 
